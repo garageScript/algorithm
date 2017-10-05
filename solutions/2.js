b8565ff06340165ca19b95a6fa6d23a2f99c3a12
Alberto Lopez <alopez@llip.life>
2017-10-05 15:32:07 -0700

    Solve #2

--- solutions/2.js
+++ solutions/2.js
@@ -1,11 +1,10 @@
-const maxOfThree = (num1, num2, num3, max)=>{
+const maxOfThree = (num1, num2, num3) =>{
   if(num1 > num2 && num1 > num3){
-    max = num1;
+    return num1;
   }else if(num2 > num1 && num2 > num3){
-    max = num2;
-  }else if(num3 > num1 && num3 > num2){
-    max = num3;
+    return num2;
+  }else{
+    return num3;
   }
-  return max;
 }
 module.exports = maxOfThree;
--- test/2.js
+++ test/2.js
@@ -1,12 +1,12 @@
 const solution = require('../solutions/2.js');
 
-const test = (num1, num2, num3, max)=>{
-  if(solution(num1, num2, num3, max) === num1){
+const test = (num1, num2, num3)=>{
+  if(solution(num1, num2, num3) === num1){
     console.log('Number 1 is max: ' + num1);
-  }else if(solution(num1,num2,num3, max) === num2){
+  }else if(solution(num1,num2,num3) === num2){
     console.log('Number 2 is max ' + num2);
-  }else if(solution(num1, num2, num3, max) === num3){
-    console.log('Number 3 is max', max);
+  }else if(solution(num1, num2, num3) === num3){
+    console.log('Number 3 is max', num3);
   }
 }
 
