b33aab68b2f1e06b8680c8616ea4e8239f230e26
mari <maricrisbonzo@gmail.com>
2017-10-05 15:31:44 -0700

    Algorithm Problem #1: Count the # of 9

--- test/1.js
+++ test/1.js
@@ -2,12 +2,15 @@ const expect = require('chai').expect;
 const solution = require('../solutions/1');
 // solution = require('../yourSolution').solution;
 
-const test = (a, b, result) => {
-  if(solution(a,b) === result) {
-    console.log(`Correct`);
+const test = (a, result) => {
+  if(solution(a) === result) {
+    console.log(`Correct!`);
   } else {
     console.log(`Wrong. Expected:`, result);
   }
 };
 
-test(5,6, 11);
+test('5son9b9', 2);
+test('99n9b9', 4);
+test('5hellowa', 1);
+test('90912nine', 3);
