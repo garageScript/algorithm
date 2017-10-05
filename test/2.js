aae01d5fc986aff33bdfc5101e2116348426aa26
Alberto Lopez <alopez@llip.life>
2017-10-05 15:40:10 -0700

    Solve #2 update 2

--- test/2.js
+++ test/2.js
@@ -5,9 +5,9 @@ const test = (num1, num2, num3)=>{
     console.log('Number 1 is max: ' + num1);
   }else if(solution(num1,num2,num3) === num2){
     console.log('Number 2 is max ' + num2);
-  }else if(solution(num1, num2, num3) === num3){
+  }else{
     console.log('Number 3 is max', num3);
   }
 }
 
-test(1,7,5);
+test(1,7,10);
