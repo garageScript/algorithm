034e8b1808eb5d2a7d2d99c4a5862999e7070390
mari <maricrisbonzo@gmail.com>
2017-10-05 17:39:13 -0700

    Fixed test file.

--- test/2.js
+++ test/2.js
@@ -5,11 +5,9 @@ const test = (num1, num2, num3, result) => {
   if(solution(num1, num2, num3) === result) {
     console.log(`Correct!`);
   } else {
-    console.log(`Wrong Result: ${solution(num1, num2, num3)}. Expected: ${result}`);
+    console.log(`Wrong. Expected: ${result}`);
   }
 };
 
 test(1,2,3,3);
 test(0,40,1,40);
-test(1,2,4,'Testing Wrong');
-test(0,30,1,'Testing Wrong');
