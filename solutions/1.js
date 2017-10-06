18973e72b7b4022d5e13127136b4051d67c3425f
Kinue Santos <kinue@llip.life>
2017-10-06 10:56:03 -0700

    deleting

--- solutions/1.js
+++ solutions/1.js
@@ -1,10 +1,1 @@
-const biggest = (num1,num2,num3) => {
-  if(num1>num2 && num1>num3){
-    return num1;
-  }
-  else if(num2>num3){
-    return num2;
-  }
-  return num3;
-};
-module.exports = biggest;
+
