71b1bf4de5d3679999326f1d8854f229a53b844b
Madhu <madhub@llip.life>
2017-10-05 20:43:00 -0700

    change in the test file

--- test/2.js
+++ test/2.js
@@ -1,17 +1,12 @@
 const test = require('../solutions/2.js')
-const testing = (a,b,c) =>{
-  if(test(a,b,c) === a){
-    console.log(a,"is greatest number");
-  }
-  if(test(a,b,c) === b){
-    console.log(b,"is greatest number");
-  }
-  if(test(a,b,c) === c){
-    console.log(c,"is greatest number");
+const testing = (a,b,c,max) =>{
+  if(test(a,b,c) === max){
+    console.log("this is the maximum");
+  }else{
+    console.log("this is not the maximum");
   }
 }
-
-testing(1,1,2);
-testing(1,2,1);
-testing(2,1,10);
+testing(1,1,2,2);
+testing(1,2,1,2);
+testing(2,1,10,2);
 
