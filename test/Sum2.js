cf790c0d53c9c6e45f8692b597dd22aaad04f2c8
Saumya Singh <saumyasing@gmail.com>
2017-10-06 16:01:32 -0700

    adding more test cases

--- test/Sum2.js
+++ test/Sum2.js
@@ -5,8 +5,10 @@ const test=(a,b,final)=>{
     console.log('Correct');
   }
     else{
-      console.log('Incorrect'+final);
+      console.log('Incorrect '+final);
     }
 }
 
 test(5,6,11);
+test(5,10,15);
+test(6,6,11);
