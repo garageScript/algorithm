782042219938c9c4951de9be4f34e368e942bfe9
mari <maricrisbonzo@gmail.com>
2017-10-09 15:00:06 -0700

    Made changes suggested by Song

--- solutions/3.js
+++ solutions/3.js
@@ -2,8 +2,7 @@ const solution = (num, hello = '', i = 0) => {
   if(i == num){
     return hello;
   }
-  hello = hello + 'hello';
-  return solution(num, hello, i + 1);
+  return solution(num, hello + 'hello', i + 1);
 };
 
 module.exports = solution;
