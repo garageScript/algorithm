377efffd45ccf7fc0e937cf0f5d832cdc034c26a
Saumya Singh <saumyasing@gmail.com>
2017-10-09 14:52:18 -0700

    fixed Max3

--- solutions/Max3.js
+++ solutions/Max3.js
@@ -1,14 +1,12 @@
 const Max3=(x,y,z)=>{
   if(z>y && z>x){
     return z;
-  }else if (y>x && y<z){
-    return y;
-  }
+  }e
   else if(y>x && y>z){
     return y;
   }
-  else if (x>y &&x>z){
-      return x;
+  else {
+    return x;
   }
 }
 module.exports = Max3;
