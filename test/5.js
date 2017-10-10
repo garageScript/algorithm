65a0c4ae9dd35cb20ed4839a90e8d8aa34319855
mari <maricrisbonzo@gmail.com>
2017-10-09 17:56:36 -0700

    The PERFECT solution.

--- test/5.js
+++ test/5.js
@@ -1,18 +1,16 @@
 const solution = require('../solutions/5');
 
-let count = 0;
-const func = () => {
-  count = count + 1;
-}
-
 const test = (num) => {
+  let count = 0;
+  const func = () => {
+    count = count + 1;
+  }
   solution(func, num);
   if(num == count){
     console.log('Correct!');
   } else {
     console.log('Wrong.');
   }
-  count = 0;
 }
 
 test(5);
