b6d90f92215a18a0cc2d84aec9bbf139371fc74f
mari <maricrisbonzo@gmail.com>
2017-10-09 18:30:32 -0700

    Updates Song wants

--- solutions/4.js
+++ solutions/4.js
@@ -1,8 +1,6 @@
 const solution = (str, num, i = 0, newStr = '') => {
-  if(i == num){
-    return newStr;
-  }
-  return solution(str, num, i + 1, str + ` ${newStr}`);
+  if (i == num) return newStr;
+  return solution(str, num, i + 1, `${str} ${newStr}`);
 };
 
 module.exports = solution;
