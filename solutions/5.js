48a7aa005f0d4afed574131f8652f55e5427b4f5
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-10-09 20:11:17 -0700

    Completed

---
+++ solutions/5.js
@@ -0,0 +1,11 @@
+/*
+Write a function that given a function and a number,
+call that function that number of times
+
+*/
+
+const callX = (func, n) => {
+  if (n === 0) return;
+  func();
+  return callX(func, n - 1);
+};
