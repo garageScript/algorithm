7ce3c858baec70d995cc7409b130880418e6aca8
mari <maricrisbonzo@gmail.com>
2017-10-12 11:53:40 -0700

    the correct solution

--- solutions/7.js
+++ solutions/7.js
@@ -1,13 +1,12 @@
-const solution = (num, i = 1, bigD = num) => {
+const solution = (num, i = 1, bigD = -1) => {
   if (i < num) {
     if (num % i == 0) {
-        bigD = i;
-    }
       if (bigD < i) {
-        return solution(num, i + 1, bigD);
+        bigD = i;
       }
     }
-  };
+    return solution(num, i + 1, bigD);
+  }
   return bigD;
 };
 module.exports = solution;
