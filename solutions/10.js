65c42554d1d2ec51b1128be0da750ba33562b134
mari <maricrisbonzo@gmail.com>
2017-10-11 19:22:44 -0700

    made changes thx to song

--- solutions/10.js
+++ solutions/10.js
@@ -1,9 +1,4 @@
 const solution = (func, num1, num2, bigger, i = 0) => {
-  if (num1 > num2) {
-    bigger = num1;
-  } else {
-    bigger = num2;
-  }
   if (i !== bigger) {
     func(i + 1);
     solution(func, num1, num2, bigger, i + 1);
--- test/10.js
+++ test/10.js
@@ -3,12 +3,22 @@ const solution = require('../solutions/10');
 const test = (testNum1, testNum2) => {
   let count = 0;
   let compare = true;
+  let bigger;
+
+  if (testNum1 > testNum2) {
+    bigger = testNum1;
+  } else {
+    bigger = testNum2;
+  }
+
   const testFunc = (x) => {
     count += 1;
     if (count !== x) compare = false;
   };
-  solution(testFunc, testNum1, testNum2);
-  if (compare && (count == testNum1 || count == testNum2)) {
+
+  solution(testFunc, testNum1, testNum2, bigger);
+
+  if (compare && (count == bigger)) {
     console.log('Correct!');
   } else {
     console.log('Wrong.');
@@ -18,3 +28,4 @@ const test = (testNum1, testNum2) => {
 test(4, 5);
 test(20, 1);
 test(0, -1);
+test(5, 10);
