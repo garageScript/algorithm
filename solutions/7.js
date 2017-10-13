f53994c40e2d2066d484e594179e604a3a27c0b2
mari <maricrisbonzo@gmail.com>
2017-10-12 21:04:41 -0700

    small change thanks to eman

--- solutions/7.js
+++ solutions/7.js
@@ -1,6 +1,6 @@
-const solution = (num, i = 1, bigD = -1) => {
+const solution = (num, i = 1, bigD) => {
   if (i < num) {
-    if (num % i == 0 && bigD < i) {
+    if (num % i == 0) {
       bigD = i;
     }
     return solution(num, i + 1, bigD);
