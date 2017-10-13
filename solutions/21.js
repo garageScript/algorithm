288fd03f9412f8363cadb4c3d2ca8103fc70c07b
Bharath <bharath@llip.life>
2017-10-12 18:28:21 -0700

    Made changes - Solved #21.js

--- solutions/21.js
+++ solutions/21.js
@@ -1,17 +1,9 @@
 const fact = (num,i=num-1) => {
-  if (num == 0){
-    return 1;            // 0! = 1
-  }else if (num <0){
-    return;              //not defined for negative numbers
+  if (num <= 0){
+    return 1;
+  }else{
+    return num * fact(num-1);
   }
-
-  if(i==0){
-    return num;
-  }
-  if(i>0){
-    num = num * i;
-  }
-  return fact(num,i-1);
 }
 
 module.exports = fact;
