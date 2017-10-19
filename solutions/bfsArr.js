7c25e05b0d46a771da4e10a95d9a948a17deff3b
Alberto Lopez <alopez@llip.life>
2017-10-19 15:51:37 -0700

    Solve bfs

--- solutions/bfsArr.js
+++ solutions/bfsArr.js
@@ -1,20 +1,19 @@
 const tree = {
   val: 10,
-  children: [{val:10, children:[{val:1},{val:2}]},{val:20, children:[{val:3},{val:4}]}]
+  children: [{val:1, children:[{val:3,children:[]},{val:4,children:[]}]},{val:2, children:[{val:5,children:[]},{val:6,children:[]}]}]
 };
 
 const bfs = (lev=[])=>{
   if(!lev.length){
     return;
-  }
-  n = lev.shift();
-  //console.log(n.getData());
-  bfs(lev.concat(n.children));
+ }
+  n = lev.pop();
+  console.log(n);
+  return bfs(lev.concat(n.children));
 }
-bfs([tree]);
+console.log(bfs([tree]));
 
 /*
-
 const dfs = (lev=[])=>{
 if(!lev.length){
 return;
