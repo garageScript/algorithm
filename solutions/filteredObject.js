3a71bd09170c2dc573666bcf941379b30623923c
Alberto Lopez <alopez@llip.life>
2017-10-27 01:10:09 -0700

    filterobjects

--- solutions/filteredObject.js
+++ solutions/filteredObject.js
@@ -4,30 +4,17 @@ const objA = {
   'zolo': "4thech"
 }
 
-const funct = (k=Object.keys(objA), v=Object.values(objA), i=0)=>{
-  /*
+const funct = (k, v, objectC={}, i=0)=>{
   if(i === k.length){
-    return;
-  }
-  */
-  if(v[i].length > 3){
-    return true;
-  }else{
-    return false;
-  }
-  // return funct(k, v, i+1);
-}
-
-const filterObj = (obj, funct, objectC={}, i=0)=>{
-  /*
-  if(i === Object.keys(obj).length){
     return objectC;
   }
-  if(funct){
-    objectC[obj[i]] = obj[obj[i]];
+  if(v[i].length > 3){
+    objectC[k[i]]=v[i];
   }
-  return (obj, funct, objectC, i+1);
-  */
-  console.log(funct());
+  return funct(k, v, objectC, i+1);
 }
-console.log(filterObj(objA));
+
+const filterObj = (obj, func, i=0)=>{
+  return func(Object.keys(obj), Object.values(obj));
+}
+console.log(filterObj(objA,funct));
